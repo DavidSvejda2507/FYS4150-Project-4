1202f411f57f8ad241bb948530c5d3e3e7e35baf
david <david.svejda2000@gmail.com>
2022-11-04 12:11:59 +0100

    Implemented the time step method and reading observables

--- Include/Ising_grid.hpp
+++ Include/Ising_grid.hpp
@@ -5,24 +5,32 @@
 #include <armadillo>
 #include <iomanip>
 #include <string>
+#include <random>
 
 class Ising_grid
 {
     private:
         arma::Mat<short> grid;
+        int L;
         double T;
         double dE [5];
+        std::mt19937 generator;
+        std::uniform_real_distribution<double> uniform;
+        std::uniform_int_distribution<int> index;
 
+        short Get_Energy(int i, int j);
+        short Get_Spin(int i, int j);
 
     public:
 
         Ising_grid(){}
 
-        Ising_grid(int L, double T);
+        Ising_grid(int L, double T, int seed);
 
+        double Get_eps();
+        double Get_m();
 
-
-
+        void Do_Spin_Flip();
 
 
 
--- Source/Ising_grid.cpp
+++ Source/Ising_grid.cpp
@@ -1,15 +1,68 @@
 #include "Ising_grid.hpp"
-
+#include <iostream>
 
 using namespace arma;
 using namespace std;
 
 
-Ising_grid::Ising_grid(int L, double T){
-    grid = Mat<short>(L, L);
+Ising_grid::Ising_grid(int L_, double T, int seed){
+    grid = Mat<short>(L_, L_);
+    L = L_;
+    generator = mt19937(seed);
+    uniform_int_distribution<short> spin(0,1); 
+    for (int i = 0; i < L; i++){
+        for (int j = 0; j < L; j++){
+            grid(i,j) = spin(generator);
+        }
+    }
+    uniform = uniform_real_distribution<double>(0,1);
+    index = uniform_int_distribution<int>(L, 2*L-1);
+
     // dE = {1,1,1,1,1};
     for (int i = 0; i < 5; i++){
-        if (i<3){dE[i] = 1;}
-        else{dE[i] = exp(-2/T);}
+        if (i>1){dE[i] = 1;}
+        else{dE[i] = exp(-2*(2-i)/T);}
+    }
+    for (int i = 0; i<5; i++){
+        cout << dE[i] << endl;
+    }
+}
+
+double Ising_grid::Get_eps(){
+    int sum = 0;
+    for (int i = L; i < 2L; i++){
+        for (int j = L; j < 2L; j++){
+            sum += Get_Energy(i, j);
+            // The energy return by Get_Energy is in the range [0, 4]
+            // To correct this we shift by -2 at the end
+        }
+    }
+    return (((double)sum/grid.n_elem)-2);
+}
+
+double Ising_grid::Get_m(){
+    return accu(grid)/grid.n_elem;
+}
+
+short Ising_grid::Get_Energy(int i, int j){
+    short s = Get_Spin(i, j);
+    short E = 0;
+    E += s^Get_Spin(i+1, j  );
+    E += s^Get_Spin(i-1, j  );
+    E += s^Get_Spin(i  , j+1);
+    E += s^Get_Spin(i  , j-1);
+    return E;
+}
+
+short Ising_grid::Get_Spin(int i, int j){
+    return grid(i%L, j%L);
+}
+
+void Ising_grid::Do_Spin_Flip(){
+    int i = index(generator);
+    int j = index(generator);
+    double chance = dE[Get_Energy(i, j)];
+    if (uniform(generator) < chance){
+        grid(i%L, j%L) = Get_Spin(i,j) ^ 1;
     }
 }
