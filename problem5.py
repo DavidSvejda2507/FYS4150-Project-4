4640039c2b2a94c588a5bd124e667e1d0d779a3b
Vibi31 <vrvichu0@gmail.com>
2022-11-22 17:34:57 +0100

    updated labels and titles, rezised scales

--- problem5.py
+++ problem5.py
@@ -8,14 +8,20 @@ for T in ("1","2.4"):
         data.append((np.genfromtxt(f"Data/Problem5/T_{T}_A_{A}.txt", delimiter='\t', skip_header=1), T, A))
         
     
-observables = ["eps","abs m","Cv","chi"]
+observables = ["Energy per spin (\u03B5) for T = 1 [J/K]","Absolute magnetisation per spin (|m|)","Specific heat capacity (Cv)","Susceptibility (\u03A7)"]
+imgnm = ["epsilon", "abs_m", "Cv", "chi"]
+xl = ["\u03B5", "|m|", "Cv", "\u03A7"]
+#yl = [" ", " ", " ", " "]
 for i in range(4):
     plt.figure()
     for j in range(4):
-        plt.plot(data[j][0][:,i], label = f"T={data[j][1]}  A={data[j][2]}")
+        plt.plot(data[j][0][:5000,i], label = f"T={data[j][1]}  A={data[j][2]}")  #plotting epsilon 
     plt.legend()
     
     plt.title(observables[i])
-    plt.savefig(f"Data/Problem5/{observables[i]}.png")
+    plt.ylabel(xl[i])
+    plt.xlabel("Number of montecarlo cycles")
+    plt.show()
+    plt.savefig(f"Data/Problem5/{imgnm[i]}.png")
         
     
